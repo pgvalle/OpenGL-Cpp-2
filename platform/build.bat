79ef8e67e7e7cd683cc2a7d4cd2bed1dfce46a60
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-29 11:51:13 -0300

    using raw git clone to compile example instead of git modules

--- platform/build.bat
+++ platform/build.bat
@@ -18,16 +18,11 @@ goto :script
 
 call :command_exists git
 
-:: is this a git repo?
-if not exist ".git" (
-  echo This directory is not a git repository.
-  exit /b 1
-)
-
-:: Are submodules updated?
+:: Make sure glfw is downloaded
 echo ================= Initializing ===================
-git submodule foreach "git reset --hard"
-git submodule update --init --recursive
+if not exist "vendor\glfw\.git" (
+  git clone --branch 3.4 https://github.com/glfw/glfw vendor/glfw
+)
 
 call :command_exists cmake
 
@@ -35,18 +30,16 @@ if not exist "build" (
   mkdir build
 )
 
-cd build
-
 :: Is project configured
 if not exist "build\Makefile" (
   echo ================= Configuring ===================
-  cmake .. -G %*
+  cmake -G %* -B build
 )
 
 :: Build
 echo ================= Building ===================
-cmake --build .
+cmake --build build
 
 echo ================= Done ===================
-echo USAGE: .\platform\build.bat generator_in_double_quotes
+echo USAGE: .\platform\build.bat cmake_generator_in_double_quotes
 echo NOTE: Run cmake --help for more info on available generators.
--- platform/build.sh
+++ platform/build.sh
@@ -6,33 +6,26 @@ command_exists() {
         exit 1
     fi
 }
+
 command_exists git
 
-# is this a git repo?
-if [ ! -d ".git" ]; then
-    echo "This directory is not a git repository."
-    exit 1
+# make sure glfw is downloaded
+echo "================= Initializing ==================="
+if [ ! -d "vendor/glfw/.git" ]; then
+    git clone --branch 3.4 https://github.com/glfw/glfw vendor/glfw
 fi
 
-# are submodules updated?
-echo "================= Initializing ==================="
-git submodule foreach "git reset --hard"
-git submodule update --init --recursive
-
 command_exists cmake
-
-mkdir -p build
-cd build
 
 # Is the project configured?
 if [ ! -f "build/Makefile" ]; then
     echo "================= Configuring ==================="
-    cmake ..
+    cmake -B build
 fi
 
 # build
 echo "================= Building ==================="
-cmake --build .
+cmake --build build
 
 echo "================= Done ==================="
 echo "NOTE: Don't forget to install xorg/wayland dev packages."
