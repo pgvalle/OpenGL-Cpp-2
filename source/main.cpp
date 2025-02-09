2b86032ec952ba8c2a3a75dc78a112a8b3caafe4
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-09 13:50:54 -0300

    fixing example

--- source/main.cpp
+++ source/main.cpp
@@ -2,9 +2,8 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
-#include <glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <gtc/matrix_transform.hpp>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 static const struct
 {
